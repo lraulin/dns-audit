89fa482295a87f306bbf7e2502fd9b00c0405d96
Lee Raulin <leeraulin@gmail.com>
2019-03-13 14:13:01 -0400

    ...

--- sql/find_diffs.sql
+++ sql/find_diffs.sql
@@ -1,6 +1,6 @@
 DROP TABLE IF EXISTS temp.a;
 CREATE TEMP TABLE a AS
-SELECT record_timestamp, domain_id, record_values
+SELECT record_timestamp, domain_id, record_values, raw
 FROM tbl_record
 WHERE 
   run_id = (
@@ -19,7 +19,7 @@ FROM
 
 DROP TABLE IF EXISTS temp.b;
 CREATE TEMP TABLE b AS
-SELECT record_timestamp, domain_id, record_values
+SELECT record_timestamp, domain_id, record_values, raw
 FROM tbl_record
 WHERE 
   run_id = (
@@ -29,7 +29,7 @@ FROM
   tbl_record
   );
 
-SELECT a.domain_id, a.record_timestamp AS timestamp_A, a.record_values AS values_A, b.record_values AS values_B, b.record_values AS timestamp_B
+SELECT a.domain_id, a.record_values AS values_A, a.raw as raw_A, b.record_values AS values_B, b.record_values AS timestamp_B, b.raw AS raw_B
 FROM a
   INNER JOIN b on a.domain_id = b.domain_id
 WHERE values_A <> values_B;
--- src/index.js
+++ src/index.js
@@ -47,42 +47,47 @@ const insertIntoTblRunDatetime = epoch =>
     .run().lastInsertRowid;
 
 const sqlInsertIntoTblRecord = db.prepare(
-  `INSERT INTO tbl_record (run_id, record_timestamp, domain_id, record_values) VALUES (?, ?, ?, ?);`
+  `INSERT INTO tbl_record (run_id, domain_id, record_values, raw) VALUES (?, ?, ?, ?);`
 );
 
-const insertIntoTblRecord = ({ runId, recordTimestamp, domainId, records }) =>
-  sqlInsertIntoTblRecord.run(runId, recordTimestamp, domainId, records)
-    .lastInsertRowid;
+const insertIntoTblRecord = ({ runId, domainId, records, raw }) =>
+  sqlInsertIntoTblRecord.run(runId, domainId, records, raw).lastInsertRowid;
+
+const parseDigForRecordValues = digOutput => {
+  const lines = digOutput.split("\n");
+  const answers = {};
+  let inAnswerSection = false;
+  for (let line of lines) {
+    if (inAnswerSection) {
+      if (line === "") break;
+      else {
+        const answer = line.split(/\s/);
+        const recordType = answer[3];
+        const value = answer[4];
+        if (types.includes(recordType)) {
+          if (!answers[recordType]) answers[recordType] = [];
+          answers[recordType].push(value);
+        }
+      }
+    }
+    if (line === ";; ANSWER SECTION:") inAnswerSection = true;
+  }
+  Object.values(answers).forEach(el => el.sort());
+  return answers;
+};
 
 const getDomainRecords = async (runId, domain) => {
   try {
     const records = [];
     // Execute dig command.
-    const res = await dig(["127.0.0.1", domain, "ANY"]);
-    if (Array.isArray(res.answer)) {
-      // Filter list to contain only desired record types. (A, NS, etc)
-      const answers = res.answer.filter(answer => {
-        return types.includes(answer.type);
-      });
-      // If there are any records of the desired type
-      if (answers.length) {
-        const records = {};
-        for (let answer of answers) {
-          if (!records[answer.type]) records[answer.type] = [];
-          records[answer.type].push(answer.value);
-        }
-        for (let type in records) {
-          records[type] = records[type].sort().join("|");
-        }
-        insertIntoTblRecord({
-          runId: runId,
-          domainId: domainIdLookup[domain],
-          recordTimestamp:
-            new Date(res.datetime).toISOString().split(".")[0] + "Z",
-          records: JSON.stringify(records)
-        });
-      }
-    }
+    const raw = await dig(["152.120.225.240", domain, "ANY"], { raw: true });
+
+    insertIntoTblRecord({
+      runId: runId,
+      domainId: domainIdLookup[domain],
+      records: JSON.stringify(parseDigForRecordValues(raw)),
+      raw
+    });
   } catch (e) {
     console.log(e);
   }
@@ -101,45 +106,104 @@ const getRecordsForAllDomains = async () => {
   }
 };
 
+const parseDigGetAnswers = digOutput => {
+  const lines = digOutput.split("\n");
+  const answers = {};
+  let inAnswerSection = false;
+  for (let line of lines) {
+    if (inAnswerSection) {
+      if (line === "") break;
+      else {
+        const answer = line.split(/\s/);
+        const recordType = answer[3];
+        const value = answer[4];
+        if (types.includes(recordType)) {
+          if (!answers[recordType]) answers[recordType] = [];
+          answers[recordType].push(line);
+        }
+      }
+    }
+    if (line === ";; ANSWER SECTION:") inAnswerSection = true;
+  }
+  return answers;
+};
+
+const unique = (arr1, arr2) => {
+  const unique1 = arr1.filter(o => arr2.indexOf(o) === -1);
+  const unique2 = arr2.filter(o => arr1.indexOf(o) === -1);
+  return unique1.concat(unique2);
+};
+
 const findMismatches = () => {
   const sqlCreateTempTables = [
     "DROP TABLE IF EXISTS temp.a;",
-    "CREATE TEMP TABLE a AS SELECT record_timestamp, domain_id, record_values FROM tbl_record WHERE run_id = ( SELECT MAX(run_id) FROM tbl_record WHERE run_id NOT IN ( SELECT MAX(run_id) FROM tbl_record ) );",
+    "CREATE TEMP TABLE a AS SELECT domain_id, record_values, raw FROM tbl_record WHERE run_id =( SELECT MAX(run_id) FROM tbl_record WHERE run_id NOT IN ( SELECT MAX(run_id) FROM tbl_record) );",
     "DROP TABLE IF EXISTS temp.b;",
-    "CREATE TEMP TABLE b AS SELECT record_timestamp, domain_id, record_values FROM tbl_record WHERE run_id = (SELECT MAX(run_id) FROM tbl_record );"
+    "CREATE TEMP TABLE b AS SELECT domain_id, record_values, raw FROM tbl_record WHERE run_id =( SELECT MAX(run_id) FROM tbl_record);"
   ];
   const sqlGetMismatches =
-    "SELECT a.domain_id, a.record_timestamp AS timestamp_A, a.record_values AS values_A, b.record_timestamp AS timestamp_B, b.record_values AS values_B FROM a INNER JOIN b on a.domain_id = b.domain_id WHERE values_A <> values_B;";
+    "SELECT a.domain_id, a.record_values AS values_A, a.raw as raw_A, b.record_values AS values_B, b.record_values AS timestamp_B, b.raw AS raw_B FROM a INNER JOIN b on a.domain_id = b.domain_id WHERE values_A <> values_B;";
   const rows = db.transaction(() => {
     sqlCreateTempTables.forEach(s => db.prepare(s).run());
     return db.prepare(sqlGetMismatches).all();
   })();
+
   if (rows.length) {
     let message = "";
     for (let row of rows) {
+      message += domainIdLookup[row.domain_id] + ":\n";
       const values_A = JSON.parse(row.values_A);
       const values_B = JSON.parse(row.values_B);
-      for (let type of types) {
-        if (values_A[type] !== values_B[type]) {
-          message +=
-            `Discrepancy in ${type} record(s) for ${
-              domainIdLookup[row.domain_id]
-            }:\n` +
-            `  ${row.timestamp_A}    ${
-              values_A[type] ? values_A[type].replace("|", " ") : "No records"
-            }\n` +
-            `  ${row.timestamp_B}    ${
-              values_B[type] ? values_B[type].replace("|", " ") : "No records"
-            }\n\n`;
+      const answerLinesA = parseDigGetAnswers(row.raw_A);
+      const answerLinesB = parseDigGetAnswers(row.raw_B);
+      console.log(answerLinesA);
+      console.log(answerLinesB);
+
+      const allTypes = new Set(
+        Object.keys(values_A).concat(Object.keys(values_B).sort())
+      );
+      console.log(allTypes);
+
+      for (let type of allTypes) {
+        if (!values_A[type]) values_A[type] = [];
+        if (!values_B[type]) values_B[type] = [];
+
+        if (values_A[type].join(" ") !== values_B[type].join(" ")) {
+          const change = values_A[type].length - values_B[type].length;
+          if (change > 0) {
+            message += `${change} ${type} record${
+              change > 1 ? "s" : ""
+            } deleted\n`;
+          } else if (change < 0) {
+            message += `${Math.abs(change)} ${type} record${
+              change < -1 ? "s" : ""
+            } added\n`;
+          }
+
+          // message +=
+          //   `${differenceType} in ${type} record(s) for ${
+          //     domainIdLookup[row.domain_id]
+          //   }:\n` +
+          //   `  ${row.timestamp_A}    ${
+          //     values_A[type] ? values_A[type].replace("|", " ") : "No records"
+          //   }\n` +
+          //   `  ${row.timestamp_B}    ${
+          //     values_B[type] ? values_B[type].replace("|", " ") : "No records"
+          //   }\n\n`;
         }
       }
+      // message += "\n";
+      // message += row.raw_A += "\n";
+      // message += row.raw_B +=
+      //   "\n\n==============================================================\n";
     }
     console.log(message);
-    email({
-      subject: "DNS Log Discrepancy Report",
-      body: message,
-      to: recipients
-    });
+    // console.log(message);
+    // email({
+    //   subject: "DNS Log Discrepancy Report",
+    //   body: message,
+    //   to: recipients
+    // });
   } else {
     console.log("No discrepancies found.");
   }
